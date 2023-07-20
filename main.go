d7f6f01d33106e7dd7f4cdc24b82ecdde0974ca4
sarthakkharia <sarthakkharia@gmail.com>
2023-07-19 19:47:55 -0700

    Add ability to update a warranty #21 Ensure that UUID is always server generated when creating new items #11

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/bigpanther/warrant/models"
 	"github.com/gin-gonic/gin"
 	"github.com/gobuffalo/pop/v6"
+	"github.com/gofrs/uuid"
 )
 
 func main() {
@@ -22,6 +23,7 @@ func setupRouter(db *pop.Connection) *gin.Engine {
 	r.GET("/warranties/:id", withDb(db, warrantyByID))
 	r.POST("/warranties", withDb(db, createWarranty))
 	r.POST("/users", withDb(db, createUser))
+	r.PUT("/warranties/:id", withDb(db, editWarranty))
 	return r
 }
 
@@ -46,9 +48,48 @@ func warrantyByID(c *context.AppContext) {
 	c.IndentedJSON(http.StatusOK, w)
 }
 
+func editWarranty(c *context.AppContext) {
+	id := c.Params.ByName("id")
+	w := &models.Warranty{}
+	err := c.DB.Find(w, id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": fmt.Sprintf("id not found: %s", id),
+		})
+		log.Println(err)
+		return
+	}
+	err2 := c.BindJSON(w)
+	if err2 != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "error updating warranty",
+		})
+		log.Println(err)
+		return
+	}
+	verrs, err := c.DB.ValidateAndUpdate(w)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "error saving warranty",
+		})
+		log.Println(err)
+		return
+	}
+	if verrs.HasAny() {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": verrs,
+		})
+		log.Println(verrs)
+		return
+	}
+	c.IndentedJSON(http.StatusOK, w)
+
+}
+
 func createWarranty(c *context.AppContext) {
 	w := &models.Warranty{}
 	err := c.BindJSON(w)
+	w.ID = uuid.UUID{}
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "error creating warranty",
@@ -56,7 +97,7 @@ func createWarranty(c *context.AppContext) {
 		log.Println(err)
 		return
 	}
-	verrs, err := c.DB.ValidateAndCreate(w)
+	verrs, err := c.DB.ValidateAndCreate(w, "ID")
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "error saving warranty",
@@ -84,7 +125,7 @@ func createUser(c *context.AppContext) {
 		log.Println(err)
 		return
 	}
-	verrs, err := c.DB.ValidateAndCreate(u)
+	verrs, err := c.DB.ValidateAndCreate(u, "ID")
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "error saving user",
