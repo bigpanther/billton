2ac853c287e444e9ed82ba2870cb87512dfb8943
Harsimran Singh Maan <maan.harry@gmail.com>
2023-12-25 01:34:02 -0800

    group routes under v1

--- internal/app/routes.go
+++ internal/app/routes.go
@@ -6,21 +6,12 @@ import (
 )
 
 func setupRouter(r *gin.Engine, firebase firebase.Firebase) {
-	warranties := r.Group("warranties")
-	WarrantiesRoutes(warranties)
-	users := r.Group("users")
-	UserRoutes(users)
-	self := r.Group("self")
-	SelfRoutes(self, firebase)
-	chains := []gin.HandlersChain{
-		warranties.Handlers,
-		users.Handlers,
-		self.Handlers,
-	}
 	// Group all routes under v1
 	v1 := r.Group("v1")
-	for _, chain := range chains {
-		v1.Handlers = append(v1.Handlers, chain...)
-	}
-
+	warranties := v1.Group("warranties")
+	WarrantiesRoutes(warranties)
+	users := v1.Group("users")
+	UserRoutes(users)
+	self := v1.Group("self")
+	SelfRoutes(self, firebase)
 }
