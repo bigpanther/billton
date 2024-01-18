6dc881554e64adc6063773befcaa9a62fce2afec
Harsimran Singh Maan <maan.harry@gmail.com>
2024-01-17 21:13:33 -0800

    google signin

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,17 +1,12 @@
 package models
 
-import (
-	"github.com/google/uuid"
-	"gorm.io/gorm"
-)
-
 type User struct {
-	gorm.Model
-	ID         uuid.UUID `json:"id" gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
-	Name       string    `json:"name" gorm:"size:255;not null"`
-	Username   string    `json:"username" gorm:"size:255;not null"`
-	Email      string    `json:"email" gorm:"size:255;not null"`
-	Role       string    `json:"role" gorm:"size:50;not null"`
+	Model
+	// gorm.Model
+	Name       string `json:"name" gorm:"size:255;not null"`
+	Username   string `json:"username" gorm:"size:255;not null"`
+	Email      string `json:"email" gorm:"size:255;not null"`
+	Role       string `json:"role" gorm:"size:50;not null"`
 	Warranties []Warranty
 }
 
--- internal/models/warranty.go
+++ internal/models/warranty.go
@@ -4,17 +4,15 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 type Warranty struct {
-	gorm.Model
-	ID              uuid.UUID `json:"id" gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	Model
 	TransactionTime time.Time `json:"transaction_time"`
 	ExpiryTime      time.Time `json:"expiry_time"`
 	BrandName       string    `json:"brand_name" gorm:"size:255;not null"`
 	StoreName       string    `json:"store_name" gorm:"size:255;not null"`
 	Amount          int       `json:"amount"`
-	UserID          uuid.UUID `json:"userid" gorm:"type:uuid;not null;index"`
+	UserID          uuid.UUID `json:"user_id" gorm:"type:uuid;not null;index"`
 	User            User
 }
