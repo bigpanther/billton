a4641701724e884e3d2d024c2a3902b63e1775d7
shreesh221298 <47675756+shreesh221298@users.noreply.github.com>
2024-02-24 13:29:18 -0800

    Welcome screen

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,20 +1,41 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:warrant/warrant/view/warrant_page.dart';
+// import 'package:warrant/warrant_bloc.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+
+// void main() => runApp(const WarrantApp());
+
+// class WarrantApp extends StatelessWidget {
+//   const WarrantApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: BlocProvider(
+//         create: (_) => WarrantBloc(),
+//         child: const GoogleSignInScreen(),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:warrant/warrant/view/warrant_page.dart';
 import 'package:warrant/warrant_bloc.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'google_sign_in_screen.dart';
 
 void main() => runApp(const WarrantApp());
 
 class WarrantApp extends StatelessWidget {
-  const WarrantApp({super.key});
+  const WarrantApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BlocProvider(
-        create: (_) => WarrantBloc(),
-        child: const GoogleSignInScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Warrant App'),
+        ),
+        body: GoogleSignInScreen(), // Display GoogleSignInScreen here
       ),
     );
   }
