cc1834fcba36142215fe6a9cf18f6f00e9353bbd
Tilak Raj <tilakrajp975@gmail.com>
2024-03-20 18:06:09 -0700

    Sign up form added

--- billtonapp/lib/main.dart
+++ billtonapp/lib/main.dart
@@ -1,28 +1,92 @@
 import 'package:billtonapp/google.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
-import 'dart:io';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
   runApp(ShreeshApp());
 }
+// class ShreeshApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Welcome Screen',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: WelcomeScreen(),
+//     );
+//   }
+// }
 
+// class WelcomeScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Welcome'),
+//         actions: [
+//           IconButton(
+//             icon: Icon(Icons.login),
+//             onPressed: () {
+//               // Add your login button onPressed action here
+//             },
+//           ),
+//           IconButton(
+//             icon: Icon(Icons.notifications),
+//             onPressed: () {
+//               // Add your notification button onPressed action here
+//             },
+//           ),
+//         ],
+//       ),
+//       body: SafeArea(
+//         child: Center(
+//           child: Text('Welcome to the app!'),
+//         ),
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         currentIndex: 0, // You can set this to any value to highlight a specific button
+//         items: [
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: 'Home',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.history),
+//             label: 'History',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.add),
+//             label: 'Add Picture',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.settings),
+//             label: 'Settings',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.person),
+//             label: 'Profile',
+//           ),
+//         ],
+//         onTap: (index) {
+//           // Add your bottom navigation button tap action here
+//         },
+//       ),
+//     );
+//   }
+// }
 class ShreeshApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome Screen',
       theme: ThemeData(
-          colorScheme:
-              ColorScheme.fromSeed(seedColor: Color.fromRGBO(255, 190, 152, 1)),
-          useMaterial3: true,
-          fontFamily: "Schyler"),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromRGBO(255, 190, 152, 1)),
+        useMaterial3: true,
+      ),
       home: WelcomeScreen(),
     );
   }
@@ -35,7 +99,6 @@ class WelcomeScreen extends StatefulWidget {
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
   int _selectedIndex = 0;
-  ValueNotifier<UserCredential?> userCredential = ValueNotifier(null);
 
   static List<Widget> _widgetOptions = <Widget>[
     HomeScreen(),
@@ -56,14 +119,9 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: userCredential.value != null
-              ? Image.network(userCredential.value!.user!.photoURL!)
-              : Icon(Icons.login),
-          onPressed: () async {
-            userCredential.value = await signInWithGoogle();
-            if (userCredential.value != null) {
-              print(userCredential.value!.user!.email);
-            }
+          icon: Icon(Icons.login),
+          onPressed: () {
+            // Navigate to login screen
           },
         ),
         actions: <Widget>[
@@ -116,20 +174,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        children: [
-          Text("Thanks for choosing billton.",
-              style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20.0)),
-          Text("Let's get started!"),
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 100, 20, 0),
-            child: Image(image: AssetImage("assets/Welcome.png")),
-          ),
-        ],
-      ),
+      child: Text('Home Screen'),
     );
   }
 }
@@ -143,65 +188,366 @@ class HistoryScreen extends StatelessWidget {
   }
 }
 
-Future<UserCredential?> signInWithGoogle() async {
-  await Firebase.initializeApp();
-
-  try {
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-    final GoogleSignInAuthentication? googleAuth =
-        await googleUser?.authentication;
-
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
-    );
-
-    return await FirebaseAuth.instance.signInWithCredential(credential);
-  } on Exception catch (e) {
-    print('Exception->$e');
-    return null;
-  }
-}
-
-Future<bool> signOutFromGoogle() async {
-  try {
-    await FirebaseAuth.instance.signOut();
-    return true;
-  } on Exception catch (_) {
-    return false;
-  }
-}
-
 class AddPictureScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
+      child: Text('Add Picture Screen'),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Settings Screen'),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Profile Screen'),
+    );
+  }
+}
+
+// class ShreeshApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Welcome Screen',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: WelcomeScreen(),
+//     );
+//   }
+// }
+
+// class WelcomeScreen extends StatefulWidget {
+//   @override
+//   _WelcomeScreenState createState() => _WelcomeScreenState();
+// }
+
+// class _WelcomeScreenState extends State<WelcomeScreen> {
+//   int _selectedIndex = 0;
+
+//   static List<Widget> _widgetOptions = <Widget>[
+//     HomeScreen(),
+//     HistoryScreen(),
+//     AddPictureScreen(),
+//     SettingsScreen(),
+//     ProfileScreen(),
+//   ];
+
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: _widgetOptions.elementAt(_selectedIndex),
+//       ),
+//       appBar: AppBar(
+//         title: Text('Welcome'),
+//         actions: <Widget>[
+//           IconButton(
+//             icon: Icon(Icons.notifications),
+//             onPressed: () {
+//               // Navigate to notifications screen
+//             },
+//           ),
+//         ],
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: const <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: 'Home',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.history),
+//             label: 'History',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.add_a_photo),
+//             label: 'Add Picture',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.settings),
+//             label: 'Settings',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.person),
+//             label: 'Profile',
+//           ),
+//         ],
+//         currentIndex: _selectedIndex,
+//         onTap: _onItemTapped,
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           // Navigate to sign in screen
+//         },
+//         tooltip: 'Sign In',
+//         child: Icon(Icons.login),
+//       ),
+//       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+//     );
+//   }
+// }
+
+// class HomeScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Text('Home Screen'),
+//     );
+//   }
+// }
+
+// class HistoryScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Text('History Screen'),
+//     );
+//   }
+// }
+
+// class AddPictureScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Text('Add Picture Screen'),
+//     );
+//   }
+// }
+
+// class SettingsScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Text('Settings Screen'),
+//     );
+//   }
+// }
+
+// class ProfileScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Text('Profile Screen'),
+//     );
+//   }
+// }
+
+// class ShreeshApp extends StatelessWidget{
+//   const ShreeshApp({super.key});
+//   @override
+//   Widget build(BuildContext context){
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(255, 190, 152, 1)),
+//         useMaterial3: true,
+//       ),
+//       debugShowCheckedModeBanner: false,
+
+//       home:
+//       Scaffold(
+//       appBar: AppBar(title: const Text('Google SignIn Screen')),
+//       body:
+
+//       Column(
+//       mainAxisAlignment: MainAxisAlignment.start,
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       children: [Padding(
+//         padding: EdgeInsets.fromLTRB(0,100,0,0),
+//         child: Text(
+//           "Bye"
+//         ),
+
+//       ),
+//       FloatingActionButton(onPressed: () { print("a"); },
+//       child: Text("A"),),
+//       Text(
+//         "Big Panther"
+//       ),
+//       Text(
+//         "Abc"
+//       )],
+//     ),),);
+//   }
+// }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const GoogleSignInScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
-      children: [
-        ElevatedButton(
-          onPressed: () async {
-            File? imageFile = await getImageFromCamera();
-            if (imageFile != null) {
-              await uploadImageToFirebase(imageFile);
-            }
-          },
-          child: const Text('Take Picture & Upload'),
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
-        const SizedBox(
-          height: 20,
-        ),
-        ElevatedButton(
-          onPressed: () async {
-            File? imageFile = await getImageFromGallery();
-            if (imageFile != null) {
-              await uploadImageToFirebase(imageFile);
-            }
-          },
-          child: const Text('Upload from Gallery'),
-        ),
-      ],
-    ));
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Future<UserCredential?> signInWithGoogle() async {
+    await Firebase.initializeApp();
+
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    } on Exception catch (e) {
+      print('Exception->$e');
+      return null;
+    }
+  }
+
+  Future<bool> signOutFromGoogle() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      return true;
+    } on Exception catch (_) {
+      return false;
+    }
+  }
+
+  Future<File?> getImageFromCamera() async {
+    final picker = ImagePicker();
+    final PickedFile? pickedFile =
+        await picker.getImage(source: ImageSource.camera);
+    if (pickedFile != null) {
+      return File(pickedFile.path);
+    } else {
+      return null;
+    }
   }
 
   Future<File?> getImageFromGallery() async {
@@ -229,52 +575,5 @@ class AddPictureScreen extends StatelessWidget {
     } catch (e) {
       print('Error uploading image to Firebase: $e');
     }
-  }
-
-  Future<File?> getImageFromCamera() async {
-    final picker = ImagePicker();
-    final XFile? pickedFile =
-        await picker.pickImage(source: ImageSource.camera);
-    if (pickedFile != null) {
-      return File(pickedFile.path);
-    } else {
-      return null;
-    }
-  }
-}
-
-class SettingsScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Settings Screen'),
-    );
-  }
-}
-
-class ProfileScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Screen'),
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const GoogleSignInScreen(),
-      debugShowCheckedModeBanner: false,
-    );
   }
 }
