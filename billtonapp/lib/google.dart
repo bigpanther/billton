7c350f18d8a0ee2a703ee100eeac066b0970e2ef
sarthakkharia <sarthakkharia@gmail.com>
2024-04-01 20:35:43 -0700

    Issue #44 assignment (#47)
    Add rounded photo on login.
    Add padding for receipt form.
    
    renamed signup form to receipt form
    
    dart updates
    
    Co-authored-by: shreesh221298 <47675756+shreesh221298@users.noreply.github.com>

--- billtonapp/lib/google.dart
+++ billtonapp/lib/google.dart
@@ -1,138 +1,14 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
-
-// class GoogleSignInScreen extends StatefulWidget {
-//   const GoogleSignInScreen({Key? key}) : super(key: key);
-
-//   @override
-//   State<GoogleSignInScreen> createState() => _GoogleSignInScreenState();
-// }
-
-// class _GoogleSignInScreenState extends State<GoogleSignInScreen> {
-//   ValueNotifier userCredential = ValueNotifier('');
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text('Google SignIn Screen')),
-//       body: ValueListenableBuilder(
-//         valueListenable: userCredential,
-//         builder: (context, value, child) {
-//           return (userCredential.value == '' || userCredential.value == null)
-//               ? Center(
-//                   child: Card(
-//                     elevation: 5,
-//                     shape: RoundedRectangleBorder(
-//                         borderRadius: BorderRadius.circular(10)),
-//                     child: IconButton(
-//                       iconSize: 40,
-//                       icon: Image.network(
-//                           'https://img.icons8.com/?size=96&id=17949&format=png'),
-//                       onPressed: () async {
-//                         userCredential.value = await signInWithGoogle();
-//                         if (userCredential.value != null)
-//                           print(userCredential.value.user!.email);
-//                       },
-//                     ),
-//                   ),
-//                 )
-//               : Center(
-//                   child: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.center,
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     children: [
-//                       Container(
-//                         clipBehavior: Clip.antiAlias,
-//                         decoration: BoxDecoration(
-//                             shape: BoxShape.circle,
-//                             border:
-//                                 Border.all(width: 1.5, color: Colors.black54)),
-//                         child: Image.network(
-//                             userCredential.value.user!.photoURL.toString()),
-//                       ),
-//                       const SizedBox(
-//                         height: 20,
-//                       ),
-//                       Text(userCredential.value.user!.displayName.toString()),
-//                       const SizedBox(
-//                         height: 20,
-//                       ),
-//                       Text(userCredential.value.user!.email.toString()),
-//                       const SizedBox(
-//                         height: 30,
-//                       ),
-//                       ElevatedButton(
-//                           onPressed: () async {
-//                             bool result = await signOutFromGoogle();
-//                             if (result) userCredential.value = '';
-//                           },
-//                           child: const Text('Logout'))
-//                     ],
-//                   ),
-//                 );
-//         },
-//       ),
-//       floatingActionButton:
-//           FloatingActionButton.small(onPressed: () => print("hello")),
-//     );
-//   }
-
-//   Future<dynamic> signInWithGoogle() async {
-//     await Firebase.initializeApp();
-//     const snackBar = SnackBar(
-//       content: Text('Yay! A SnackBar!'),
-//     );
-
-//     ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
-//     try {
-//       final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-//       final GoogleSignInAuthentication? googleAuth =
-//           await googleUser?.authentication;
-
-//       final credential = GoogleAuthProvider.credential(
-//         accessToken: googleAuth?.accessToken,
-//         idToken: googleAuth?.idToken,
-//       );
-
-//       return await FirebaseAuth.instance.signInWithCredential(credential);
-//     } on Exception catch (e) {
-//       const snackBar = SnackBar(
-//         content: Text('Yay! A SnackBar!'),
-//       );
-
-//       ScaffoldMessenger.of(context).showSnackBar(snackBar);
-//       // TODO
-//       print('exception->$e');
-//     }
-//   }
-
-// // Find the ScaffoldMessenger in the widget tree
-// // and use it to show a SnackBar.
-
-//   Future<bool> signOutFromGoogle() async {
-//     try {
-//       await FirebaseAuth.instance.signOut();
-//       return true;
-//     } on Exception catch (_) {
-//       return false;
-//     }
-//   }
-// }
 import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class GoogleSignInScreen extends StatefulWidget {
-  const GoogleSignInScreen({Key? key}) : super(key: key);
+  const GoogleSignInScreen({super.key});
 
   @override
   State<GoogleSignInScreen> createState() => _GoogleSignInScreenState();
@@ -263,7 +139,8 @@ class _GoogleSignInScreenState extends State<GoogleSignInScreen> {
 
   Future<File?> getImageFromCamera() async {
     final picker = ImagePicker();
-    final PickedFile? pickedFile = await picker.getImage(source: ImageSource.camera);
+    final PickedFile? pickedFile =
+        await picker.getImage(source: ImageSource.camera);
     if (pickedFile != null) {
       return File(pickedFile.path);
     } else {
@@ -273,7 +150,8 @@ class _GoogleSignInScreenState extends State<GoogleSignInScreen> {
 
   Future<File?> getImageFromGallery() async {
     final picker = ImagePicker();
-    final PickedFile? pickedFile = await picker.getImage(source: ImageSource.gallery);
+    final XFile? pickedFile =
+        await picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
       return File(pickedFile.path);
     } else {
@@ -285,8 +163,8 @@ class _GoogleSignInScreenState extends State<GoogleSignInScreen> {
     try {
       print("uploading");
       String fileName = DateTime.now().millisecondsSinceEpoch.toString();
-      firebase_storage.Reference ref =
-          firebase_storage.FirebaseStorage.instance.ref().child('files/${FirebaseAuth.instance.currentUser!.uid}/$fileName.jpg');
+      Reference ref = FirebaseStorage.instance.ref().child(
+          'files/${FirebaseAuth.instance.currentUser!.uid}/$fileName.jpg');
       await ref.putFile(imageFile);
       String imageUrl = await ref.getDownloadURL();
       print('Image uploaded to Firebase: $imageUrl');
