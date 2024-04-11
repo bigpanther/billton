2068202f5d1cc519c2d4dd6660e04a3d90d82a49
Harsimran Singh Maan <maan.harry@gmail.com>
2024-04-10 20:57:12 -0700

    Add tests (#49)

--- billtonapp/lib/main.dart
+++ billtonapp/lib/main.dart
@@ -129,7 +129,7 @@ class HomeScreen extends StatelessWidget {
     return Center(
       child: Column(
         children: [
-          Text("Thanks for choosing billton.",
+          Text("Thanks for choosing Billton.",
               style: TextStyle(
                   color: Theme.of(context).primaryColor,
                   fontWeight: FontWeight.w500,
@@ -281,24 +281,6 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Center(
       child: Text('Profile Screen'),
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const GoogleSignInScreen(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
